f00683223f18db1b6306b7754a395c930bc49db2
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-12 17:05:11 -0300

    FEAT: Implemented routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Discovery from './pages/Discovery';
+import Home from './pages/Home';
+import Notifications from './pages/Notifications';
+import Profile from './pages/Profile';
+
 function App() {
-  return <div className="App"></div>;
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: 'home',
+          element: <Home />,
+        },
+        {
+          path: 'discovery',
+          element: <Discovery />,
+        },
+        {
+          path: 'notifications',
+          element: <Notifications />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+        {
+          path: 'profile',
+          element: <Profile />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
