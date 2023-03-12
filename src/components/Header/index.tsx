452bdc4afab91aaba247935f2a2018bb3f6e9066
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-12 17:36:27 -0300

    FEAT/STYLE: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Logo from '../../utils/icons/Logo';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    background-color: var(--color-quaternary);
+    height: 72px;
+    display: flex;
+    padding: 0 50px;
+    align-items: center;
+`;
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Logo />
+    </StyledHeader>
+  );
+}
