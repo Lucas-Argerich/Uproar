84e17aaa536ffc6139c849e611c43adaf2da8a42
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-12 19:00:32 -0300

    STYLE: Layout styling and positioning of Outlet

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Header from '../Header';
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 100px;
+  flex-grow: 1;
+`;
+
+const LeftSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const Main = styled.main`
+  flex-basis: 700px;
+  max-width: 700px;
+`;
+
+const RightSection = styled.section`
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
 
 export default function Layout() {
   return (
-    <div>
+    <Container>
       <Header />
-      <Outlet />
-    </div>
+      <Wrapper>
+        <LeftSection>a</LeftSection>
+        <Main>
+          <Outlet />
+        </Main>
+        <RightSection>a</RightSection>
+      </Wrapper>
+    </Container>
   );
 }
