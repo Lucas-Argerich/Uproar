895810044aed8f3bf764dda71b2c133ad53833c9
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-15 22:57:45 -0300

    FEAT: Google Auth implementation for user creation

---
+++ src/firebase/auth.ts
@@ -0,0 +1,17 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { app } from './app';
+
+export const auth = getAuth(app);
+
+export const loginWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(auth, provider)
+    .then((result) => {
+      //const credential = GoogleAuthProvider.credentialFromResult(result);
+      //const token = credential.accessToken;
+      return result.user;
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
