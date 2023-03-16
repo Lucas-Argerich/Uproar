0fb53ee2792f382118eb162e4736ebe47d2b0b6b
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-15 22:54:59 -0300

    FEAT: Firebase app initialization

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,13 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { app } from './app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDkbk_-i6sqLnjplW8v_XYgKfawPVcI0X0",
-  authDomain: "uproar-717c5.firebaseapp.com",
-  projectId: "uproar-717c5",
-  storageBucket: "uproar-717c5.appspot.com",
-  messagingSenderId: "807217028333",
-  appId: "1:807217028333:web:fc7281db375b6ca3cdc0e4",
-  measurementId: "G-KN5NQ47S0Q"
-};
-
-const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
