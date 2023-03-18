ba13eabe2654d7d1fa50b593113dfca315ea53df
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-18 20:21:48 -0300

    FEAT: CancelLink Component

---
+++ src/components/CancelLink/Icons/ArrowLeft.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react';
+
+export default function ArrowLeft(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={25}
+      height={24}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M15.5 19.92 8.98 13.4c-.77-.77-.77-2.03 0-2.8l6.52-6.52"
+        stroke="#EBEBEB"
+        strokeWidth={1.5}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/CancelLink/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import ArrowLeft from './Icons/ArrowLeft';
+
+interface ICancelLink {
+  to: string;
+  children?: ReactNode;
+}
+
+const StyledLink = styled(Link)`
+  align-items: center;
+  color: var(--color-light);
+  display: flex;
+  font-size: 0.875rem;
+  padding: 10px;
+  text-decoration: none;
+  width: 100%;
+`;
+
+export default function CancelLink({ to, children }: ICancelLink) {
+  return (
+    <StyledLink to={to}>
+      <ArrowLeft />
+      {children}
+    </StyledLink>
+  );
+}
