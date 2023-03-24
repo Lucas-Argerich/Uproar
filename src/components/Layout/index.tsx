c775035283118ebf74cdeb67dee5c919facd34fd
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-24 17:37:57 -0300

    FORMAT: Formatted the Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import Header from '../Header';
 import styled from 'styled-components';
 import { Outlet, useMatch, useNavigate } from 'react-router-dom';
 import Navbar from '../Navbar';
 import { useSelector } from 'react-redux';
-import { selectAuth, selectCurrentUser, selectIsLoading } from '../../redux/selectors/userSelectors';
+import {
+  selectAuth,
+  selectCurrentUser,
+  selectIsLoading,
+} from '../../redux/selectors/userSelectors';
 
 const Container = styled.div`
   display: flex;
@@ -38,23 +42,23 @@ const RightSection = styled.section`
 `;
 
 export default function Layout() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const isLoginPage = Boolean(useMatch('login'));
   const isRegisterPage = Boolean(useMatch('register'));
   const isPostPage = Boolean(useMatch('post'));
   const showSideContent = isLoginPage || isRegisterPage || isPostPage;
-  
-  const userAuth = useSelector(selectAuth)
-  const userData = useSelector(selectCurrentUser)
-  const isUserLoading = useSelector(selectIsLoading)
+
+  const userAuth = useSelector(selectAuth);
+  const userData = useSelector(selectCurrentUser);
+  const isUserLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
-    if(!isUserLoading && userAuth && !userData) {
-      navigate("/register")
+    if (!isUserLoading && userAuth && !userData) {
+      navigate('/register');
     }
-  }, [navigate, userAuth, userData, isUserLoading])
-  
+  }, [navigate, userAuth, userData, isUserLoading]);
+
   return (
     <Container>
       <Header />
