e8fe0cb0791f850c7d8092fedca742d635f763de
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-24 21:09:39 -0300

    STYLE: Nav responsive style & positioning

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import useMediaQuery from '../../hooks/useMediaQuery';
 import AboutIcon from './icons/AboutIcon';
 import DiscoveryIcon from './icons/DiscoveryIcon';
 import HomeIcon from './icons/HomeIcon';
 import NotificationIcon from './icons/NotificationIcon';
 
 const Nav = styled.nav`
+  bottom: 20px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   font-size: 1.5rem;
   gap: 15px;
+  justify-content: space-evenly;
+  left: 0;
   margin-top: 50px;
+  position: absolute;
+  width: 100%;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: column;
+    width: auto;
+    position: relative;
+  }
 `;
 
 const StyledNavLink = styled(NavLink)`
@@ -22,53 +34,50 @@ const StyledNavLink = styled(NavLink)`
 `;
 
 export default function Navbar() {
+  const mediaQuery768 = useMediaQuery('(min-width: 768px)');
   return (
     <Nav>
-      <StyledNavLink
-        to="home"
-      >
+      <StyledNavLink to="home">
         {({ isActive }: { isActive: boolean }) => (
           <>
             <HomeIcon isSelected={isActive} />
-            <span style={{ fontWeight: isActive ? 700 : 600 }}>
-                Home
-            </span>
+            {mediaQuery768 && (
+              <span style={{ fontWeight: isActive ? 700 : 600 }}>Home</span>
+            )}
           </>
         )}
       </StyledNavLink>
-      <StyledNavLink
-        to="discovery"
-      >
+      <StyledNavLink to="discovery">
         {({ isActive }: { isActive: boolean }) => (
           <>
             <DiscoveryIcon isSelected={isActive} />
-            <span style={{ fontWeight: isActive ? 700 : 600 }}>
+            {mediaQuery768 && (
+              <span style={{ fontWeight: isActive ? 700 : 600 }}>
                 Discovery
-            </span>
+              </span>
+            )}{' '}
           </>
         )}
       </StyledNavLink>
-      <StyledNavLink
-        to="notifications"
-      >
+      <StyledNavLink to="notifications">
         {({ isActive }: { isActive: boolean }) => (
           <>
             <NotificationIcon isSelected={isActive} />
-            <span style={{ fontWeight: isActive ? 700 : 600 }}>
+            {mediaQuery768 && (
+              <span style={{ fontWeight: isActive ? 700 : 600 }}>
                 Notifications
-            </span>
+              </span>
+            )}
           </>
         )}
       </StyledNavLink>
-      <StyledNavLink
-        to="about"
-      >
+      <StyledNavLink to="about">
         {({ isActive }: { isActive: boolean }) => (
           <>
             <AboutIcon isSelected={isActive} />
-            <span style={{ fontWeight: isActive ? 700 : 600 }}>
-                About
-            </span>
+            {mediaQuery768 && (
+              <span style={{ fontWeight: isActive ? 700 : 600 }}>About</span>
+            )}{' '}
           </>
         )}
       </StyledNavLink>
