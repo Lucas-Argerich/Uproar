7c1287e0d13d2d6834ea7f55b92e032d77ab9d57
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-03-24 21:02:45 -0300

    FEAT!: useMediaQuery hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function useMediaQuery(query: string): boolean {
+  const getMatches = (query: string): boolean => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+  useEffect(() => {
+    function handleChange() {
+      setMatches(getMatches(query));
+    }
+    const matchMedia = window.matchMedia(query);
+    handleChange();
+    matchMedia.addEventListener('change', handleChange);
+
+    return () => {
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [query]);
+
+  return matches;
+}
